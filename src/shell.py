30674b9d6b7617db298a1c4fe826b536e2064787
Baicheng Yin <y@yinbaicheng.com>
2023-10-27 00:32:02 +0000

    pulled out tail

--- src/shell.py
+++ src/shell.py
@@ -59,22 +59,7 @@ def eval(cmdline, out):
         elif app == "head":
             out = head(args, out)
         elif app == "tail":
-            if len(args) != 1 and len(args) != 3:
-                raise ValueError("wrong number of command line arguments")
-            if len(args) == 1:
-                num_lines = 10
-                file = args[0]
-            if len(args) == 3:
-                if args[0] != "-n":
-                    raise ValueError("wrong flags")
-                else:
-                    num_lines = int(args[1])
-                    file = args[2]
-            with open(file) as f:
-                lines = f.readlines()
-                display_length = min(len(lines), num_lines)
-                for i in range(0, display_length):
-                    out.append(lines[len(lines) - display_length + i])
+            out = tail(args, out)
         elif app == "grep":
             if len(args) < 2:
                 raise ValueError("wrong number of command line arguments")
