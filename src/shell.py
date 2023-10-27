33763095e4b0a8bb85a94e4753358a42372b1d45
Baicheng Yin <y@yinbaicheng.com>
2023-10-27 00:31:44 +0000

    pulled out head

--- src/shell.py
+++ src/shell.py
@@ -57,21 +57,7 @@ def eval(cmdline, out):
                 with open(a) as f:
                     out.append(f.read())
         elif app == "head":
-            if len(args) != 1 and len(args) != 3:
-                raise ValueError("wrong number of command line arguments")
-            if len(args) == 1:
-                num_lines = 10
-                file = args[0]
-            if len(args) == 3:
-                if args[0] != "-n":
-                    raise ValueError("wrong flags")
-                else:
-                    num_lines = int(args[1])
-                    file = args[2]
-            with open(file) as f:
-                lines = f.readlines()
-                for i in range(0, min(len(lines), num_lines)):
-                    out.append(lines[i])
+            out = head(args, out)
         elif app == "tail":
             if len(args) != 1 and len(args) != 3:
                 raise ValueError("wrong number of command line arguments")
