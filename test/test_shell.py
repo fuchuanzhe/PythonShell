a208cc2dfbcdaa56e08b1cf15202dbd95169ab3d
HOSH19 <hoshuhan@gmail.com>
2023-11-08 15:17:04 +0000

    prelim test cases added

--- test/test_shell.py
+++ test/test_shell.py
@@ -3,12 +3,225 @@ import unittest
 from shell import eval
 from collections import deque
 
+# not too sure about the paths in the test cases
 
 class TestShell(unittest.TestCase):
-    def test_shell(self):
+    # def test_shell(self):
+    #     out = deque()
+    #     eval("echo foo", out)
+    #     self.assertEqual(out.popleft(), "foo\n")
+    #     self.assertEqual(len(out), 0)
+
+    def test_cat(self):
+        out = deque()
+        eval("cat cat1.txt cat2.txt", out)
+        self.assertEqual(out.popleft(), "this is cat1.\n")
+        self.assertEqual(out.popleft(), "this is cat2.\n")
+        self.assertEqual(len(out), 0)
+
+    def test_cd(self):
+        out = deque()
+        eval("cd catTest", out)
+        self.assertEqual(len(out), 0)
+
+    def test_echo(self):
         out = deque()
         eval("echo foo", out)
         self.assertEqual(out.popleft(), "foo\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_find(self):
+        out = deque()
+        eval("find . -name findTest.txt", out)
+        self.assertEqual(out.popleft(), "findTest.txt\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_find1(self):
+        out = deque()
+        eval("find ./findTest -name findTest1.txt", out)
+        self.assertEqual(out.popleft(), "findTest1.txt\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_find2(self):
+        out = deque()
+        eval("find ./findTest -name *.txt", out)
+        self.assertEqual(out.popleft(), "findTest1.txt\n")
+        self.assertEqual(out.popleft(), "findTest2.txt\n")
+        self.assertEqual(len(out), 0)
+
+    def test_grep(self):
+        out = deque()
+        eval("grep hihi grepTest.txt", out)
+        self.assertEqual(out.popleft(), "hihi\n")
+        self.assertEqual(out.popleft(), "hihi my name\n")
+        self.assertEqual(len(out), 0)
+
+    def test_grep1(self):
+        out = deque()
+        eval("grep hihi ./grepTest1.txt", out)
+        self.assertEqual(out.popleft(), "hihi 1\n")
+        self.assertEqual(out.popleft(), "hihi my name 1\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_grep2(self):
+        out = deque()
+        eval("grep hihi grepTest.txt ./grepTest1.txt", out)
+        self.assertEqual(out.popleft(), "hihi\n")
+        self.assertEqual(out.popleft(), "hihi my name\n")
+        self.assertEqual(out.popleft(), "hihi 1\n")
+        self.assertEqual(out.popleft(), "hihi my name 1\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_head(self):
+        out = deque()
+        eval("head headTest.txt", out)
+        self.assertEqual(out.popleft(), "hihi\n")
+        self.assertEqual(out.popleft(), "HIHI\n")
+        self.assertEqual(out.popleft(), "hihi my name\n")
+        self.assertEqual(out.popleft(), "a\n")
+        self.assertEqual(out.popleft(), "b\n")
+        self.assertEqual(out.popleft(), "c\n")
+        self.assertEqual(out.popleft(), "d\n")
+        self.assertEqual(out.popleft(), "e\n")
+        self.assertEqual(out.popleft(), "f\n")
+        self.assertEqual(out.popleft(), "g\n")
+        self.assertEqual(len(out), 0)
+
+    def test_head1(self):
+        out = deque()
+        eval("head -n 2 headTest.txt", out)
+        self.assertEqual(out.popleft(), "hihi\n")
+        self.assertEqual(out.popleft(), "HIHI\n")
+        self.assertEqual(len(out), 0)
+
+    def test_head2(self):
+        out = deque()
+        eval("head -n 4 headTest.txt", out)
+        self.assertEqual(out.popleft(), "hihi\n")
+        self.assertEqual(out.popleft(), "HIHI\n")
+        self.assertEqual(out.popleft(), "hihi my name\n")
+        self.assertEqual(out.popleft(), "a\n")
+        self.assertEqual(len(out), 0)
+
+    def test_ls(self):
+        out = deque()
+        eval("ls", out)
+        self.assertEqual(out.popleft(), "catTest\n")
+        self.assertEqual(out.popleft(), "findTest\n")
+        self.assertEqual(out.popleft(), "grepTest\n")
+        self.assertEqual(out.popleft(), "findTest.txt\n")
+        self.assertEqual(out.popleft(), "grepTest.txt\n")
+        self.assertEqual(out.popleft(), "headTest.txt\n")
+        self.assertEqual(out.popleft(), "test_shell.py\n")
+        self.assertEqual(out.popleft(), "test_shell2.py\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_pwd(self):
+        out = deque()
+        eval("pwd", out)
+        self.assertEqual(out.popleft(), "/Users/hoshuhan/Documents/UCL/Y2/COMP0010 Software Engineering/code/comp0010-shell-python-p20/test\n")
+        self.assertEqual(len(out), 0)
+
+    def test_sort(self):
+        out = deque()
+        eval("sort sortTest.txt", out)
+        self.assertEqual(out.popleft(), "HIHI\n")
+        self.assertEqual(out.popleft(), "a\n")
+        self.assertEqual(out.popleft(), "b\n")
+        self.assertEqual(out.popleft(), "c\n")
+        self.assertEqual(out.popleft(), "hihi\n")
+        self.assertEqual(out.popleft(), "hihi my name\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_sort1(self):
+        out = deque()
+        eval("sort ./sortTest1.txt", out)
+        self.assertEqual(out.popleft(), "HIHI\n")
+        self.assertEqual(out.popleft(), "a\n")
+        self.assertEqual(out.popleft(), "b\n")
+        self.assertEqual(out.popleft(), "c\n")
+        self.assertEqual(out.popleft(), "hihi\n")
+        self.assertEqual(out.popleft(), "hihi my name\n")
+        self.assertEqual(len(out), 0)
+
+    def test_sort2(self):
+        out = deque()
+        eval("sort -r sortTest.txt", out)
+        self.assertEqual(out.popleft(), "hihi my name\n")
+        self.assertEqual(out.popleft(), "hihi\n")
+        self.assertEqual(out.popleft(), "c\n")
+        self.assertEqual(out.popleft(), "b\n")
+        self.assertEqual(out.popleft(), "a\n")
+        self.assertEqual(out.popleft(), "HIHI\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_sort3(self): # to check how to check output.txt
+        out = deque()
+        eval("sort -o sortTest.txt", out)
+        self.assertEqual(out.popleft(), "HIHI\n")
+        self.assertEqual(out.popleft(), "a\n")
+        self.assertEqual(out.popleft(), "b\n")
+        self.assertEqual(out.popleft(), "c\n")
+        self.assertEqual(out.popleft(), "hihi\n")
+        self.assertEqual(out.popleft(), "hihi my name\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_sort4(self):
+        out = deque()
+        eval("sort -n ./sortTest2.txt", out)
+        self.assertEqual(out.popleft(), "1\n")
+        self.assertEqual(out.popleft(), "2\n")
+        self.assertEqual(out.popleft(), "3\n")
+        self.assertEqual(out.popleft(), "4\n")
+        self.assertEqual(out.popleft(), "5\n")
+
+        self.assertEqual(len(out), 0)
+    
+    def test_sort5(self):
+        out = deque()
+        eval("sort -nr ./sortTest2.txt", out)
+        self.assertEqual(out.popleft(), "5\n")
+        self.assertEqual(out.popleft(), "4\n")
+        self.assertEqual(out.popleft(), "3\n")
+        self.assertEqual(out.popleft(), "2\n")
+        self.assertEqual(out.popleft(), "1\n")
+        self.assertEqual(len(out), 0)
+
+    def test_tail(self):
+        out = deque()
+        eval("tail", out)
+        self.assertEqual(out.popleft(), "foo\n")
+        self.assertEqual(len(out), 0)
+
+    def test_tail(self):
+        out = deque()
+        eval("tail tailTest.txt", out)
+        self.assertEqual(out.popleft(), "a\n")
+        self.assertEqual(out.popleft(), "b\n")
+        self.assertEqual(out.popleft(), "c\n")
+        self.assertEqual(out.popleft(), "d\n")
+        self.assertEqual(out.popleft(), "e\n")
+        self.assertEqual(out.popleft(), "f\n")
+        self.assertEqual(out.popleft(), "g\n")
+        self.assertEqual(out.popleft(), "h\n")
+        self.assertEqual(out.popleft(), "i\n")
+        self.assertEqual(out.popleft(), "j\n")
+        self.assertEqual(len(out), 0)
+
+    def test_tail1(self):
+        out = deque()
+        eval("tail -n 2 tailTest.txt", out)
+        self.assertEqual(out.popleft(), "i\n")
+        self.assertEqual(out.popleft(), "j\n")
+        self.assertEqual(len(out), 0)
+    
+    def test_tail2(self):
+        out = deque()
+        eval("tail -n 4 tailTest.txt", out)
+        self.assertEqual(out.popleft(), "g\n")
+        self.assertEqual(out.popleft(), "h\n")
+        self.assertEqual(out.popleft(), "i\n")
+        self.assertEqual(out.popleft(), "j\n")
         self.assertEqual(len(out), 0)
 
 
---
+++ test/test_shell2.py
@@ -0,0 +1,72 @@
+import unittest
+from parser import Parser
+
+class TestParser(unittest.TestCase):
+    def setUp(self):
+        self.parser = Parser()
+
+    def test_parse_pwd(self):
+        self.assertEqual(self.parser.parse("pwd"),["pwd"])
+
+    def test_parse_cd(self):
+        self.assertEqual(self.parser.parse("cd .."),["cd", ".."])
+
+    def test_parse_echo(self):
+        self.assertEqual(self.parser.parse("echo hi"),["echo", "hi"])
+
+    def test_parse_ls(self):
+        self.assertEqual(self.parser.parse("ls"),["ls"])
+
+    def test_parse_ls_single_arg(self):
+        self.assertEqual(self.parser.parse("ls ./src"),["ls", "./src"])
+
+    def test_parse_cat(self):
+        self.assertEqual(self.parser.parse("cat file.txt"),["cat", "file.txt"])
+
+    def test_parse_head(self):
+        self.assertEqual(self.parser.parse("head -n 15 file.txt"),["head", "-n", "15", "file.txt"])
+    
+    def test_parse_tail(self):
+        self.assertEqual(self.parser.parse("tail -n 15 file.txt"),["tail", "-n", "15", "file.txt"])
+
+    def test_parse_grep(self):
+        self.assertEqual(self.parser.parse("grep hello dir1/file1.txt dir1/file2.txt"),["grep", "hello", "15", "dir1/file1.txt", "dir1/file2.txt"])
+    
+    def test_parse_cut(self):
+        self.assertEqual(self.parser.parse("cut -b -3,5- ./src/file.txt"),["cut", "-b", "-3,5-", "./src/file.txt"])
+
+    def test_parse_find(self):
+        self.assertEqual(self.parser.parse("find . -name *.py"),["find", "./src", "-name", "*.py"])
+
+    def test_parse_uniq(self):
+        self.assertEqual(self.parser.parse("uniq -i file.txt"),["uniq", "-i", "file.txt"])
+
+    def test_parse_sort(self):
+        self.assertEqual(self.parser.parse("sort -r file.txt"),["sort", "-r", "file.txt"])
+
+    def test_parse_double_quotes(self):
+        self.assertEqual(self.parser.parse("echo \"hi\""),["echo", "\"hi\""])
+
+    def test_parse_single_quotes(self):
+        self.assertEqual(self.parser.parse("echo \'hi\'"),["echo", "\'hi\'"])
+    
+    def test_parse_semicolon(self):
+        self.assertEqual(self.parser.parse("echo hi; echo hello"),[["echo", "hi"],["echo", "hello"]])
+
+    def test_parse_command_substitution(self):
+        self.assertEqual(self.parser.parse("echo \"hi `echo hello`\""),["echo", "hi `echo hello`\""])
+
+    def test_parse_pipe(self):
+        self.assertEqual(self.parser.parse("cat dir/text1.txt | grep hello"),["cat", "dir/text1.txt", "|", "grep", "hello"])
+
+    def test_parse_output_redirection(self):
+        self.assertEqual(self.parser.parse("echo hi > file.txt"),["echo", "hi", ">", "file.txt"])
+
+    def test_parse_input_redirection(self):
+        self.assertEqual(self.parser.parse("echo hi > file.txt"),["echo", "hi", ">", "file.txt"])
+
+    def test_parse_semicolon_pipe(self):
+        self.assertEqual(self.parser.parse("echo hi; cat dir/text1.txt | grep hello"),[["echo", "hi"], ["cat", "dir/text1.txt", "|", "grep", "hello"]])
+
+if __name__ == "__main__":
+    unittest.main()
