50364dec18fa367ee273380d5864b9421b0e73d5
HOSH19 <hoshuhan@gmail.com>
2023-11-10 09:40:49 +0000

    all testcases working except cd

--- src/commands/find.py
+++ src/commands/find.py
@@ -13,19 +13,14 @@ def find(args, out):
     if len(args) < 3:
         for line in sys.stdin:
             print(line.strip())
-    
-    # for a in args:
-    #     print(a)
 
     dir = args[0]
     file = args[2]
-    # print(args[2])
-    # print(index)
 
     if '*' in file:
         for filename in os.listdir(dir):
             # print(filename + "1")
-            if file[1:] == filename[-len(file[1:])]:
+            if file[1:] == filename[-len(file[1:]):]:
                 out.append(dir + "/" + filename + "\n")
     else:
         for filename in os.listdir(dir):
