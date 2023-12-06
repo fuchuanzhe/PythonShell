904e20edf27fd765cbef4b440477dfb55b1093df
fuchuanzhe <fuchuanzhe03@gmail.com>
2023-12-06 00:36:36 +0000

    added doc for flatten_virtual_input

--- src/commands/flatten_list/flatten_virtual_input.py
+++ src/commands/flatten_list/flatten_virtual_input.py
@@ -2,6 +2,15 @@ from collections import deque
 
 
 def flatten_virtual_input(input_list):
+    """
+    Flatten a list of strings by separating into different lines.
+
+    Parameters:
+    - input_list (deque): List of strings to be flattened.
+
+    Returns:
+    - result (deque): A deque containing flattened strings with preserved line breaks.
+    """
     result = deque()
     for string in input_list:
         lines = string.split("\n")
